3ee62236c77f0ccb24222830e4611db5c4d98f9f
dheeraj <dheerajmlv027@gmail.com>
2024-01-24 11:25:39 +0530

    anagram solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,10 @@
 */
 
 function isAnagram(str1, str2) {
+  sortedStr1 = str1.toLowerCase().split('').sort().join('');
+  sortedStr2 = str2.toLowerCase().split('').sort().join('');
 
+  return sortedStr1==sortedStr2;
 }
 
 module.exports = isAnagram;
